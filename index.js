41f4ec0273ed109a07e59159e1794d44065930d4
unknown <00225919@uca.edu.sv>
2025-02-13 20:06:44 -0600

    Cambios Finales

---
+++ index.js
@@ -0,0 +1,28 @@
+const yesBtn = document.querySelector('#yesBtn');
+const noBtn = document.querySelector('#noBtn');
+const popup = document.querySelector('.popup');
+const closeBtn = document.querySelector('.close-btn');
+
+yesBtn.addEventListener('click', function () {
+    popup.style.display = 'flex'; // Mostrar el pop-up
+});
+
+closeBtn.addEventListener('click', function () {
+    popup.style.display = 'none'; // Ocultar el pop-up
+});
+
+function moveNoBtn() {
+    const viewportWidth = window.innerWidth - noBtn.clientWidth;
+    const viewportHeight = window.innerHeight - noBtn.clientHeight;
+
+    const randomX = Math.random() * viewportWidth;
+    const randomY = Math.random() * viewportHeight;
+
+    noBtn.style.position = "absolute";
+    noBtn.style.left = `${randomX}px`;
+    noBtn.style.top = `${randomY}px`;
+}
+
+noBtn.addEventListener('mouseover', moveNoBtn);
+noBtn.addEventListener('touchstart', moveNoBtn); // Para móviles
+
